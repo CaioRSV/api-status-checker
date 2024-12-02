af01aac696085dc0385152f66a62e3d1c82a158a
CaioRSV <caiorobertosv@gmail.com>
2024-12-02 09:23:04 -0300

    wip animate viewMode change

--- app/components/apiList.tsx
+++ app/components/apiList.tsx
@@ -1,75 +1,26 @@
-import React from 'react'
+'use client';
 
+import React, { useState } from 'react'
+import { motion } from "framer-motion";
 
 type ApiListProps = {
     viewGridMode: "onePerLine" | "manyPerLine"
 }
 
 const ApiList = ({viewGridMode}: ApiListProps) => {
+  //to fix not correct logic to make the div animate flex changes
+  const [viewMode, setViewMode] = useState<"onePerLine"|"manyPerLine">(viewGridMode);
   return (
-    <div className={`w-full h-full bg-red-800`}>
-        <p>{viewGridMode}</p>
-        {/* <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
+    <motion.div className={`w-full h-full bg-red-800`} style={{
+      ...(viewGridMode=="manyPerLine" ? {
+        display: 'flex'
+      } : {})
+    }}>
         <p>{viewGridMode}</p>
         <p>{viewGridMode}</p>
         <p>{viewGridMode}</p>
         <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p>
-        <p>{viewGridMode}</p> */}
-    </div>
+    </motion.div>
   )
 }
 
